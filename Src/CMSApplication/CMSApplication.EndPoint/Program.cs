daba8711c000689c3c4af666132c0c097e3eeb70
Ali Rashtbari <dev.alirashtbari@gmail.com>
2022-03-08 17:25:05 +0330

    seting up the layout and some other options.

--- Src/CMSApplication/CMSApplication.EndPoint/Program.cs
+++ Src/CMSApplication/CMSApplication.EndPoint/Program.cs
@@ -1,27 +1,85 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+ConfigureContainer(builder);
+
+
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+Configure(app);
+
+
+
+
+
+// --- method --- //
+
+
+
+void ConfigureServices(IServiceCollection services)
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+
+    services.AddControllersWithViews();
+
+
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 
-app.UseRouting();
 
-app.UseAuthorization();
+void Configure(WebApplication builder)
+{
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
 
-app.Run();
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+
+    app.UseEndpoints(endpoints =>
+    {
+
+        endpoints.MapControllerRoute(
+                name : "areas",
+                pattern : "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+            );
+
+
+        endpoints.MapControllerRoute(
+                name : "default",
+                pattern : "{controller=Home}/{action=Index}/{id?}"
+            );
+
+    });
+
+
+    app.Run();
+}
+
+
+
+void ConfigureContainer(WebApplicationBuilder builder)
+{
+
+    builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+    builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+    {
+       
+    });
+
+}
